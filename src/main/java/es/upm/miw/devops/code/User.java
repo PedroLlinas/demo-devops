91cf8328835e001009ae5492adbfb7b8850c9f32
Pedro Llinas Ferrer <pllinas@MacBook.local>
2025-09-12 19:53:39 +0200

    User class creation #1

---
+++ src/main/java/es/upm/miw/devops/code/User.java
@@ -0,0 +1,27 @@
+package es.upm.miw.devops.code;
+
+public class User {
+    private String name;
+    private String familyName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public User(String name, String familyName) {
+        this.name = name;
+        this.familyName = familyName;
+    }
+}
